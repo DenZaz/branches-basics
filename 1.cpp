bf13f6fc59bd1a641b6a0483bb5e6d4a4b2185e7
Даниил Зазвонов < >
2017-03-16 20:37:39 +0700

    Zavershenie

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-
 #define N 19
+
+int main(int argc, char *argv[])
+{    
+;
+     system("CLS"); 
+     int i;
+     float S;
+     for (S=0, i=1; i<=N; i += 2) 
+            S+=log(i * 0.1);
+     printf ("S=%f\n",S);
+     system("PAUSE");
+     return 0;
+}
+
+
